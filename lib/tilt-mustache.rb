98e8a95b0fe49b8fe80363b144a8f456ceef1916
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2012-01-04 18:20:23 +0100

    Bump to version 0.0.1

--- lib/tilt-mustache.rb
+++ lib/tilt-mustache.rb
@@ -1,13 +1,11 @@
 require 'tilt' unless defined? Tilt
+require 'mustache'
 
 module Tilt
   # Mustache template
 	# https://github.com/defunkt/mustache
 	class MustacheTemplate < Template
-		def initialize_engine
-			return if defined? ::Mustache
-			require_template_library 'mustache'
-		end
+		def initialize_engine; end
 
 		def prepare; end
 
